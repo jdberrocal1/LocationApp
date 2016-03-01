07640b5a09cd394836729d07d2a9b3d31c12b24a
Daniel Berrocal <jdanielb1992@gmail.com>
2016-03-01 09:43:57 -0600

    Track location

--- location/www/js/controller.js
+++ location/www/js/controller.js
@@ -2,7 +2,7 @@
  * Created by jberrocal-as on 29/02/2016.
  */
 angular.module('starter.controllers', ['ionic'])
-  .controller('MapCtrl', ['$scope', function($scope) {
+  .controller('MapCtrl', ['$scope','$timeout','$cordovaGeolocation', function($scope,$timeout,$cordovaGeolocation) {
     $scope.user = {};
 
     $scope.saveDetails = function(){
@@ -10,64 +10,50 @@ angular.module('starter.controllers', ['ionic'])
       var lgt = $scope.user.longitude;
       var des = $scope.user.desc;
 
-      // Code to write to Firebase will be here
     }
 
+    var posOptions = {timeout: 3000, enableHighAccuracy: true};
+    var flag=false;
 
-  }])
+    $scope.onTimeout = function(){
+      $cordovaGeolocation.getCurrentPosition(posOptions)
+        .then(function (position) {
 
-  .directive('map', function($cordovaGeolocation) {
-    return {
-      restrict: 'A',
-      link:function(scope, element, attrs){
+          var myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+          mapOptions = {
+            zoom: 13,
+            center: myLatlng
+          };
+          if(!flag) {
+            map = new google.maps.Map(document.getElementById("map"), mapOptions);
+            flag=true;
+          }else{
+            marker.setMap(null);
+          }
 
-        var pos={
-          zValue: scope.$eval(attrs.zoom),
-
-        lat: scope.$eval(attrs.lat),
-        lng: scope.$eval(attrs.lng)
-      }
-        var posOptions = {timeout: 3000, enableHighAccuracy: false};
-        $cordovaGeolocation
-          .getCurrentPosition(posOptions)
-          .then(function (position) {
-            pos.lat  = position.coords.latitude
-            pos.lng = position.coords.longitude
-
-
-            var myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude),
-              mapOptions = {
-                zoom: pos.zValue,
-                center: myLatlng
-              },
-              map = new google.maps.Map(element[0],mapOptions);
-
-            marker = new google.maps.Marker({
-              position: myLatlng,
-              map: map,
-              draggable:true
-            });
-
-            google.maps.event.addListener(marker, 'dragend', function(evt){
-
-              scope.$parent.user.latitude = evt.latLng.lat();
-              scope.$parent.user.longitude = evt.latLng.lng();
-              scope.$apply();
-
-              console.log('Current Latitude:',evt.latLng.lat(),'Current Longitude:',evt.latLng.lng());
-
-            });
-
-
-          }, function(err) {
-            // error
-            console.log(err);
+          marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            draggable:true
           });
 
 
+          google.maps.event.addListener(marker, 'dragend', function(evt){
+
+            console.log('Current Latitude:',evt.latLng.lat(),'Current Longitude:',evt.latLng.lng());
+
+          });
+
+
+        }, function(err) {
+          // error
+          console.log(err);
+        });
+      mytimeout = $timeout($scope.onTimeout,5000);
+    }
+    var mytimeout = $timeout($scope.onTimeout,5000);
 
 
 
-      }
-    };
-  });
+  }]);
+
